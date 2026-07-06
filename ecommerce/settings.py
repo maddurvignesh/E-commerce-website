54309384e8b6437404fdd992322a1a7b8388db08
Vignesh Maddur <maddurvignesh059@gmail.com>
2026-07-06 19:16:41 +0530

    Add Vercel deployment config

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3!-&97dhy-&vho_!&^)oh2rt1d5u&mex$8b8le49*_w-7g7lq0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
